e16fd1f2759c69863341a9040d2502b47d92792f
OlgaOFrolova <50freesmart@gmail.com>
2023-05-18 18:44:34 +0300

    commit

---
+++ tests/test_ping_api.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://restful-booker.herokuapp.com/ping"
+
+class TestPing:
+    def test_ping(self):
+        response = requests.get(url)
+        expected_status_code = 201
+        assert response.status_code == expected_status_code, f"Unexpected status code! Expected {expected_status_code}. " \
+                                                             f"Actual:{response.status_code} API is`not running "
